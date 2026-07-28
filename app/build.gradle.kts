7cacee3bda67bccac3a3472d9171ead19f727b33
LorenzoAmadio <lorenzoamadio04@gmail.com>
2026-07-28 18:38:16 +0200

    per grafica dinamica

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig= true
+    }
+    
 }
 
 dependencies {
