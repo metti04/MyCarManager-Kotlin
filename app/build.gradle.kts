59cb5a2d520a43e5528d3a02c63aaa661b389d2d
metti04 <73652589+metti04@users.noreply.github.com>
2026-07-30 16:21:11 +0200

    Aggiunto home e item_le_tue_auto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.androidx.cardview)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.recyclerview)
     implementation(libs.material)
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
