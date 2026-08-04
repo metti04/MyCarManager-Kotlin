0f3db62f6af66bb4e8376a7d5ba86f84fc063c7b
metti04 <73652589+metti04@users.noreply.github.com>
2026-08-04 18:42:11 +0200

    fragment finiti

---
+++ app/src/main/java/com/example/mycarmanager/RecyclerViewAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.mycarmanager
+
+/*
+import android.view.View
+import androidx.glance.visibility
+
+class RecyclerViewAdapter {
+    holder.itemView.setOnClickListener {
+        val dett = holder.binding.llDettagliEspansi
+        dett.visibility = if (dett.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+    }
+}*/
