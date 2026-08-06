959fe36e867d6f3d00ba8d56b25c65aeda261079
labclexHUB <fiorettialessio@proton.me>
2026-08-06 17:46:28 +0200

    bottone aggiunta auto

--- app/src/main/java/com/example/mycarmanager/ui/auto/CensimentoAutoFragment.kt
+++ app/src/main/java/com/example/mycarmanager/ui/auto/CensimentoAutoFragment.kt
@@ -1,33 +1,33 @@
-package com.example.mycarmanager.ui.home
+package com.example.mycarmanager.ui.auto
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.mycarmanager.R
-import com.example.mycarmanager.ui.auto.CensimentoAutoFragment
+import com.example.mycarmanager.ui.home.HomeFragment
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
-class HomeFragment : Fragment() {
+class CensimentoAutoFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the XML layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_censimento_auto, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // Initialize UI elements here using 'view.findViewById'
-        val efabAddVeicolo = view.findViewById<ExtendedFloatingActionButton>(R.id.efabAddVeicolo)
-        efabAddVeicolo.setOnClickListener {
+        val btnAnnulla = view.findViewById<Button>(R.id.btnAnnulla)
+        btnAnnulla.setOnClickListener {
             parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, CensimentoAutoFragment())
+                .replace(R.id.fragment_container, HomeFragment())
                 .addToBackStack(null) // Allows user to hit Back to return to HomeFragment
                 .commit()
         }
